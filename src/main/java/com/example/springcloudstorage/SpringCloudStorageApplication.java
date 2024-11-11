7d95b71e24065cdbcc47df31769937c76e19b154
ac5295 <ac5295@student.jamk.fi>
2024-11-11 21:52:12 +0200

    project startup, dependencies

---
+++ src/main/java/com/example/springcloudstorage/SpringCloudStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcloudstorage/SpringCloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
